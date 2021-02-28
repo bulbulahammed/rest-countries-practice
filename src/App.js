8f154f0c5e0b293102ae3f2e2ae9499344edbf98
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2021-02-28 20:31:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(response => response.json())
+      .then(data => {
+        setCountries(data)
+        const names = data.map(country => country.name)
+      })
+      .catch(error => console.log(error))
+
+  }, [])
+
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart)
+  };
+
   return (
     <div className="App">
+      <h1>{countries.length}</h1>
+      <h4>Country Added:{cart.length}</h4>
+      <Cart cart={cart}></Cart>
+      {
+        countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
+      }
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
